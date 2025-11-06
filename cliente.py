f04825cc0e06a18cd129c7e7d078bf85180bf876
Miguelin04 <miguel.a.luna@unl.edu.ec>
2025-11-06 10:42:40 -0500

    añadir el documento

--- cliente.py
+++ cliente.py
@@ -10,7 +10,7 @@ class Cliente:
     #Constructor de la clase
     def __init__(self, nombre = "Cliente" ):
         self.nombre        = nombre
-        self.num_articulos = random.randint(10, 15) # Ahora el mínimo es 10
+        self.num_articulos = random.randint(10, 15) 
         self.metodo_pago   = ""
         self.articulos     = []
         self.tiempo_total_atencion = 0 # Nuevo: para guardar el resultado final
